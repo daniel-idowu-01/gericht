1f6ca719af3cc6e660e3dd19f93d39a13dc1b699
daniel-idowu-01 <danielidowu414@gmail.com>
2024-01-07 19:29:24 +0100

    clean code in navbar.js

--- src/Nav.js
+++ src/Nav.js
@@ -6,7 +6,7 @@ import { VscChromeClose } from "react-icons/vsc"
 
 const Nav = () => {
 
-    /* Navigation Bar Toggle */
+    // navigation Bar Toggle
     const [showSideBar, setShowSideBar] = useState(false)
     const navLinkStyle = "hidden md:block text-base hover:opacity-75"
 
@@ -14,13 +14,35 @@ const Nav = () => {
         setShowSideBar(!showSideBar)
     }
 
-    //to close sidebar when a sidebar link is clicked
+    // to close sidebar when a sidebar link is clicked
     const closeBar = () => {
         setShowSideBar(false)
     }
 
+    const links = [
+        {
+            id: 1,
+            linkName: 'Home',
+            link: '/'
+        },
+        {
+            id: 2,
+            linkName: 'Food',
+            link: '/food'
+        },
+        {
+            id: 3,
+            linkName: 'Contact Us',
+            link: '/contact'
+        },
+        {
+            id: 4,
+            linkName: 'Blog',
+            link: '/blog'
+        },
+    ]
     return (
-        <nav className="flex justify-between p-10 sm:px-32 bg-black text-white">
+        <nav className="flex justify-between p-10 md:px-32 bg-black text-white">
             <div>
                 <Link to='/'> <p className="text-xl font-bold">GERICHT</p> </Link>
             </div>
@@ -28,15 +50,23 @@ const Nav = () => {
             <div>
                 {/* Desktop View sideBar */}
                 <ul className="flex gap-14">
-                    <li className={navLinkStyle}> <Link to="/">Home</Link> </li>
-                    <li className={navLinkStyle}> <Link to="/food">Food</Link> </li>
-                    <li className={navLinkStyle}> <Link to="/contact">Contact Us</Link> </li>
-                    <li className={navLinkStyle}> <Link to="/blog">Blog</Link> </li>
+                    {
+                        links.map(link => (
+                            <li key={link.id} className={navLinkStyle}>
+                                <Link to={link.link}>{link.linkName}</Link>
+                            </li>
+                        ))
+                    }
                     <button
                         className="md:hidden text-2xl px-3 py-0.5"
                         onClick={sideBar}
                     >
-                        {showSideBar ? <VscChromeClose /> : <FaBars />}
+                        {showSideBar
+                            ?
+                            <VscChromeClose />
+                            :
+                            <FaBars />
+                        }
                     </button>
                 </ul>
             </div>
@@ -46,12 +76,16 @@ const Nav = () => {
             </div>
 
             {/* Mobile View sideBar */}
-            <div className={` ${showSideBar ? 'block' : 'hidden'} space-y-8 md:hidden absolute top-16 right-0.5 bg-black p-12 px-16 h-fit w-80 pb-20 text-center items-center ` }> {/* String literals */}
-                <Link onClick={closeBar} to="/" className="block text-lg">Home</Link>
-                <Link onClick={closeBar} to="/food" className="block text-lg">Food</Link> 
-                <Link onClick={closeBar} to="/contact" className="block text-lg">Contact</Link>
-                <Link onClick={closeBar} to="/blog" className="block text-lg">Blog</Link>
-                <Link onClick={closeBar} to="/login" className="block text-lg">Login / Register</Link>
+            <div
+                className={` ${showSideBar ? 'block' : 'hidden'} space-y-8 md:hidden absolute top-16 right-0.5 bg-black p-12 px-16 h-fit w-80 pb-20 text-center items-center `}
+            >
+                {
+                    links.map(link => (
+                        <Link onClick={closeBar} to={link.link} className="block text-lg">
+                            {link.linkName}
+                        </Link>
+                    ))
+                }
             </div>
 
         </nav>
