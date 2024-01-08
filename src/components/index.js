6760dc7318c441da9b735cd0e1f222c0a1695656
daniel-idowu-01 <danielidowu414@gmail.com>
2024-01-08 18:09:15 +0100

    created component for What we believe

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import Header from "./Header";
 import About from "./About";
 import TodaySpecialComp from "./TodaySpecialComp";
+import WhatWeBelieve from "./WhatWeBelieve";
 
 export {
-  Header, About, TodaySpecialComp
+  Header, About, TodaySpecialComp, WhatWeBelieve
 }
