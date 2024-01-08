3a2c58ac9f7f898608110a95c2c0f757f09d7695
daniel-idowu-01 <danielidowu414@gmail.com>
2024-01-08 18:18:52 +0100

    created component for Awards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,4 @@
 import Design from './images/Vector.svg'
-import ListGroup from './ListGroup';
-import Food1 from './images/food-1.jpg'
 import Food2 from './images/food-2.png'
 import Food3 from './images/food-3.png'
 import Food4 from './images/food-4.png'
@@ -8,7 +6,7 @@ import Food5 from './images/food-5.png'
 import yellowBg from './images/yellowBg.png'
 import ContactImg from './images/contact-img.png'
 import '../index.css'
-import { Header, About, TodaySpecialComp, WhatWeBelieve, VideoComp } from '../components';
+import { Header, About, TodaySpecialComp, WhatWeBelieve, VideoComp, Awards } from '../components';
 
 
 function Home() {
@@ -28,27 +26,7 @@ function Home() {
             <VideoComp />
 
             {/* Awards and Recognition */}
-            <section className='bg-bgColor flex flex-col sm:flex-row gap-10 justify-around p-10 sm:py-40 sm:px-32'>
-                
-                <div className='text-center md:text-left mt-10 md:mt-0'>
-                    <p>Awards & recognition</p>
-                    <img alt='' src={Design} className='absolute left-1/2 w-5 h-5 md:relative md:left-0' />
-                    <h2 className='text-goldcolor text-4xl mt-5 mb-10'>Our Laurels</h2>
-                    
-                    <div className='flex justify-between gap-5'>
-                        <ListGroup name='Rising Star' desc='Lorem ipsum dolor sit amet, consectetur.' />
-                        <ListGroup name='Outstanding Chef' desc='Lorem ipsum dolor sit amet, consectetur.' />   
-                    </div>
-                    <div className='flex mt-10 gap-5'>
-                        <ListGroup name='Bib Gourmond' desc='Lorem ipsum dolor sit amet, consectetur.' />
-                        <ListGroup name='AA Hospitality ' desc='Lorem ipsum dolor sit amet, consectetur.' />   
-                    </div>
-                </div>
-
-                <div className='shrink-0 relative grid place-items-center'>
-                    <img alt='' src={Food1} className='h-96 w-96 z-40 relative' />
-                </div>
-            </section>
+            <Awards />
 
             {/* Photo Gallery */}
             <section className='p-10 sm:py-40 sm:px-32 md:flex md:justify-between'>
