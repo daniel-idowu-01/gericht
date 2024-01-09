31ddd747b77c9644715945363fcbb538c3b31313
daniel-idowu-01 <danielidowu414@gmail.com>
2024-01-10 00:13:40 +0100

    changed index to post.id in blogcomp images

--- src/pages/BlogComp.jsx
+++ src/pages/BlogComp.jsx
@@ -6,10 +6,10 @@ function BlogComp() {
     return (
         <div className="bg-black h-full">
             <p className="text-4xl text-goldcolor text-center">Blog</p>
-                { data.posts.map((post, index) => (
+                { data.posts.map(post => (
                     <div key={post.id} className='p-5 mt-10 border mx-5 md:mx-40 '> 
                         <h2 className="text-goldcolor text-2xl text-center mb-5"> {post.title} </h2>
-                        <img src= {post.imgSrc} key={index} alt=''  className='object-cover w-full mb-5 h-96' />
+                        <img src= {post.imgSrc} alt=''  className='object-cover w-full mb-5 h-96' />
                         <p className="text-white w-fit md:p-5 mb-5"> {post.content}</p>
                         <div className="flex justify-between">
                             <p className="text-white opacity-50 text-sm">By {post.author} </p>
